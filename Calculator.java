d1420a895efbc2100bea2e265b5d27a15a7914ba
ariba450 <aribachow123@gmail.com>
2026-01-18 01:00:19 +0600

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.practiceproblem1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alvir
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        
+         Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        
+        char c=scan.next().charAt(0);
+        
+        if(c =='+'){
+            System.out.println(a+b);
+        }
+        else if(c =='-'){
+            System.out.println(a-b);
+        }
+                else if(c =='*'){
+            System.out.println(a*b);
+        }
+             else if(c =='/'){
+            System.out.println(a/b);
+        }
+        else
+             {
+                 System.out.println("Invalid");
+             }
+        
+        
+    }
+}

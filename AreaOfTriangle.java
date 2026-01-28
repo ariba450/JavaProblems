7f0f68902ce2061242a4c964ea2d8b3593f5245a
ariba450 <aribachow123@gmail.com>
2026-01-28 22:04:41 +0600

    Update AreaOfTriangle.java

--- AreaOfTriangle.java
+++ AreaOfTriangle.java
@@ -1,15 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.practiceproblem01;
 
 import java.util.Scanner;
 
-/**
- *
- * @author alvir
- */
+
 public class AreaOfTriangle {
 
     public static void main(String[] args) {

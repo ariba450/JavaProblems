1fe1dd5864f90804f86df05981e92a16b14cbf44
ariba450 <aribachow123@gmail.com>
2026-01-28 22:04:10 +0600

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.practiceproblem1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author alvir
- */
+
 public class Calculator {
 
     public static void main(String[] args) {
